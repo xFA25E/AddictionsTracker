65fe595c05d1c33da938b34fb4662f990a41b998
Valeriy Litkovskyy <vlr.ltkvsk@protonmail.com>
2023-02-02 13:10:29 +0100

    Implement context menus and ui improvements

--- src/MainWindow.axaml.cs
+++ src/MainWindow.axaml.cs
@@ -17,6 +17,7 @@ public partial class MainWindow : Window
     int dayWidth = 2;
 
     List<Addiction> addictions;
+    List<(Border, Border)> addictionPositions = new();
 
     public MainWindow()
     {
@@ -33,7 +34,7 @@ public partial class MainWindow : Window
         }
     }
 
-    private void zoom(int delta)
+    void zoom(int delta)
     {
         Grid aGrid = addictionsGrid;
         int newDayWidth = Math.Max(dayWidth + delta, 2);
@@ -48,26 +49,72 @@ public partial class MainWindow : Window
                 {
                     if (s is Rectangle rect)
                     {
-                        rect.SetValue(Canvas.LeftProperty, rect.GetValue(Canvas.LeftProperty) / dayWidth * newDayWidth);
+                        rect.SetValue(Canvas.LeftProperty, rect.GetValue(Canvas.LeftProperty) / (dayWidth + 1) * (newDayWidth + 1));
                         rect.Width = newDayWidth;
                     }
                     else if (s is TextBlock label)
                     {
-                        label.SetValue(Canvas.LeftProperty, label.GetValue(Canvas.LeftProperty) / dayWidth * newDayWidth);
+                        label.SetValue(Canvas.LeftProperty, label.GetValue(Canvas.LeftProperty) / (dayWidth + 1) * (newDayWidth + 1));
                     }
                 }
             }
             else if (c.Classes.Contains("failures"))
             {
-                foreach (Rectangle f in ((StackPanel) ((Border) c).Child).Children)
+                foreach (var f in ((StackPanel) ((Border) c).Child).Children)
                 {
-                    f.Width = f.Width / dayWidth * newDayWidth;
+                    if (f is Rectangle rect)
+                    {
+                        rect.Width = rect.Width / (dayWidth + 1) * (newDayWidth + 1);
+                    }
+                    else if (f is Border b)
+                    {
+                        ((Rectangle) b.Child).Width = ((Rectangle) b.Child).Width / dayWidth * newDayWidth;
+                    }
                 }
             }
         }
         aGrid.EndBatchUpdate();
 
         dayWidth = newDayWidth;
+    }
+
+    private void swapRows(int rowA, int rowB)
+    {
+        Grid aGrid = addictionsGrid;
+
+        aGrid.BeginBatchUpdate();
+        foreach (Control c in aGrid.Children)
+        {
+            if (Grid.GetRow(c) == rowA)
+            {
+                Grid.SetRow(c, rowB);
+            }
+            else if (Grid.GetRow(c) == rowB)
+            {
+                Grid.SetRow(c, rowA);
+            }
+        }
+        aGrid.EndBatchUpdate();
+
+        var temp = addictions[rowA];
+        addictions[rowA] = addictions[rowB];
+        addictions[rowB] = temp;
+    }
+
+    private void moveUp(int row)
+    {
+        if (row > 1)
+        {
+            swapRows(row, row - 1);
+        }
+    }
+
+    private void moveDown(int row)
+    {
+        if (row < addictions.Count)
+        {
+            swapRows(row, row + 1);
+        }
     }
 
     public void CreateControls()
@@ -116,7 +163,7 @@ public partial class MainWindow : Window
                      date > minDate;
                      date = date.AddMonths(-1))
                 {
-                    var left = dateNow.Subtract(date).TotalDays * dayWidth - dayWidth;
+                    var left = dateNow.Subtract(date).TotalDays * (dayWidth + 1);
 
                     var label = new TextBlock();
                     label.Text = date.ToString("MM/yy");
@@ -137,8 +184,11 @@ public partial class MainWindow : Window
         }
 
         // ADDICTIONS AND FAILURES
-        addictions.Each(1, (addiction, row) =>
+
+        int row = 0;
+        foreach (var addiction in addictions)
         {
+            row++;
             aGrid.RowDefinitions.Add(new RowDefinition(GridLength.Auto));
 
             // ADDICTION
@@ -149,12 +199,53 @@ public partial class MainWindow : Window
                 border.SetValue(Grid.RowProperty, row);
                 aGrid.Children.Add(border);
 
+                var dockPanel = new DockPanel();
+                border.Child = dockPanel;
+
                 var addictionLabel = new TextBlock();
                 addictionLabel.Text = addiction.Title;
-                addictionLabel.VerticalAlignment = Avalonia.Layout.VerticalAlignment.Center;
-                addictionLabel.HorizontalAlignment = Avalonia.Layout.HorizontalAlignment.Right;
+                addictionLabel.SetValue(DockPanel.DockProperty, Dock.Right);
                 addictionLabel.Padding = new Avalonia.Thickness(8);
-                border.Child = addictionLabel;
+                dockPanel.Children.Add(addictionLabel);
+
+                var editButton = new Button();
+                editButton.Content = "*";
+                editButton.SetValue(DockPanel.DockProperty, Dock.Left);
+                dockPanel.Children.Add(editButton);
+
+                // Context Menu
+                {
+                    var addFailureMI = new MenuItem();
+                    addFailureMI.Header = $"Add Failure to {addiction.Title}";
+
+                    var editAddictionMI = new MenuItem();
+                    editAddictionMI.Header = $"Edit {addiction.Title}";
+
+                    var deleteAddictionMI = new MenuItem();
+                    deleteAddictionMI.Header = $"Delete {addiction.Title}";
+
+                    var moveUpMI = new MenuItem();
+                    moveUpMI.Header = "Move Up";
+                    moveUpMI.Click += (s, a) => moveUp(Grid.GetRow(border));
+
+                    var moveDownMI = new MenuItem();
+                    moveDownMI.Header = "Move Down";
+                    moveDownMI.Click += (s, a) => moveDown(Grid.GetRow(border));
+
+                    editButton.ContextMenu = new ContextMenu()
+                    {
+                        Items = new Control[]
+                        {
+                            addFailureMI,
+                            editAddictionMI,
+                            deleteAddictionMI,
+                            moveUpMI,
+                            moveDownMI,
+                            new MenuItem() { Header = "Close Menu" }
+                        },
+                    };
+                    editButton.Click += (s, a) => editButton.ContextMenu.Open();
+                }
             }
 
             // FAILURES
@@ -171,30 +262,57 @@ public partial class MainWindow : Window
                 failures.Orientation = Avalonia.Layout.Orientation.Horizontal;
                 border.Child = failures;
 
+                // TODAY
+                {
+                    var todayArea = new Rectangle();
+                    todayArea.Width = dayWidth + 1;
+                    failures.Children.Add(todayArea);
+
+                    var latestFailure = addiction.Failures.Min?.FailedAt;
+                    if (latestFailure != null && latestFailure.Equals(dateNow))
+                    {
+                        todayArea.Fill = new SolidColorBrush(Color.Parse("Red"));
+                    }
+                    else
+                    {
+                        todayArea.Fill = new SolidColorBrush(Color.Parse("Green"));
+                    }
+                }
+
                 var previousFailureDate = dateNow;
                 foreach (var failure in addiction.Failures)
                 {
-                    var abstinencePeriod =
-                        previousFailureDate.Subtract(failure.FailedAt);
+                    var abstinenceDays = previousFailureDate
+                        .Subtract(failure.FailedAt)
+                        .TotalDays - 1;
                     previousFailureDate = failure.FailedAt;
 
-                    var abstinenceArea = new Rectangle();
-                    abstinenceArea.Width = abstinencePeriod.TotalDays * dayWidth - dayWidth;
-                    abstinenceArea.Fill = new SolidColorBrush(Color.Parse("Green"));
-                    failures.Children.Add(abstinenceArea);
+                    if (abstinenceDays != 0)
+                    {
+                        var abstinenceArea = new Rectangle();
+                        abstinenceArea.Width = abstinenceDays * (dayWidth + 1);
+                        abstinenceArea.Fill = new SolidColorBrush(Color.Parse("Green"));
+                        abstinenceArea.SetValue(ToolTip.TipProperty, $"Abstained for {abstinenceDays} day{(abstinenceDays == 1 ? "" : "s")} from {addiction.Title}");
+                        abstinenceArea.SetValue(ToolTip.ShowDelayProperty, 15);
+                        failures.Children.Add(abstinenceArea);
+                    }
+
+                    var failureBorder = new Border();
+                    failureBorder.BorderBrush = new SolidColorBrush(Color.Parse("Black"));
+                    failureBorder.BorderThickness = new Avalonia.Thickness(1, 0, 0, 0);
+                    failureBorder.SetValue(ToolTip.TipProperty, $"At {failure.FailedAt.ToString("yyyy-MM-dd")} failed {addiction.Title}");
+                    failureBorder.SetValue(ToolTip.ShowDelayProperty, 15);
+                    failures.Children.Add(failureBorder);
 
                     var failureArea = new Rectangle();
                     failureArea.Width = dayWidth;
                     failureArea.Fill = new SolidColorBrush(Color.Parse("Red"));
-                    failures.Children.Add(failureArea);
+                    failureBorder.Child = failureArea;
                 }
             }
         }
-        );
 
         aGrid.EndBatchUpdate();
-
-        Console.WriteLine("{0}", this.Bounds);
     }
 }
 
@@ -204,13 +322,12 @@ public static class Extensions
 
     public static TimeSpan Subtract(this DateOnly a, DateOnly b)
     {
-        return a.ToDateTime(zeroTime) - b.ToDateTime(zeroTime);
+        return a.ToDateTime() - b.ToDateTime();
 
     }
 
-    public static void Each<T>(this IEnumerable<T> ie, int start, Action<T, int> action)
+    public static DateTime ToDateTime(this DateOnly d)
     {
-        var i = start;
-        foreach (var e in ie) action(e, i++);
+        return d.ToDateTime(zeroTime);
     }
 }
